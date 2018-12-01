3df92fc4f9bfedebc7df2ab4ba98ce63ff8b3e9d
Stephan Griesel <sgriesel@gmail.com>
2018-12-01 08:05:57 +0100

    add pages & folders

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Me</h1>
+      <p>This is the about me page</p>
+    </div>
+  </Layout>
+)
+
+export default About
---
+++ src/pages/company/team.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../../components/layout'
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>The Team</h1>
+      <p>This is the about me page</p>
+    </div>
+  </Layout>
+)
+
+export default Team
