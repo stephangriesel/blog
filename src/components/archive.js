14f30826e6a492abf9ec460f7aaa6f2edc443c37
Stephan Griesel <sgriesel@gmail.com>
2018-12-07 06:43:43 +0100

    update post limit & order

--- src/components/archive.js
+++ src/components/archive.js
@@ -3,7 +3,10 @@ import { StaticQuery, graphql, Link } from 'gatsby'
 
 const POST_ARCHIVE_QUERY = graphql`
   query BlogPostArchive {
-    allMarkdownRemark {
+    allMarkdownRemark(limit:5, sort: {
+        order: DESC
+        fields:[frontmatter___date]
+      }) {
       edges {
         node {
           frontmatter {
