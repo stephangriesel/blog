20522e1395f603ed1af390f5acd4e08418c760a0
Stephan Griesel <sgriesel@gmail.com>
2018-12-24 15:41:44 +0100

    add post

--- src/components/layout.js
+++ src/components/layout.js
@@ -56,8 +56,8 @@ const Layout = ({ children, location }) => (
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <Spring 
-        from={{height: location.pathname === '/' ? 100 : 380 }}
-        to={{height: location.pathname === '/' ? 380 : 130 }}
+        from={{height: location.pathname === '/' ? 200 : 380 }}
+        to={{height: location.pathname === '/' ? 480 : 230 }}
         >
           {styles => (
             <div style={{overflow:'hidden', ...styles}}>
